b87dce4f3f13654457d7ea6033cd217100b6c843
Poker Arena Dev <dev@poker-arena.com>
2025-11-26 20:07:29 +0330

    feat: add database connection and migration setup

---
+++ internal/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"go-poker-arena/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Database connected successfully")
+	return db, nil
+}
+
+func Migrate(db *gorm.DB) error {
+	return db.AutoMigrate(
+		&models.User{},
+		&models.Room{},
+		&models.Game{},
+	)
+}
